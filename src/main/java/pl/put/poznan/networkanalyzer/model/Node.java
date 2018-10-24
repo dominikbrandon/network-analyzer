1a3eda17ddb0ef1157581e2ab488528e5b713786
Dominik Grzelak <grzelak.dominik@gmail.com>
2018-10-24 17:51:21 +0200

    Add Node template class

---
+++ src/main/java/pl/put/poznan/networkanalyzer/model/Node.java
@@ -0,0 +1,11 @@
+package pl.put.poznan.networkanalyzer.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Node {
+}
