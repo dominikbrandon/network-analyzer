994364e0b02a95610156cd688b82d4f4be9bf480
Pawel Myszkowski <pawel.myszkowsky@gmail.com>
2018-10-27 20:52:47 +0200

    Wsadzenie klas do osobnych plikow
    Node, TypeNode i Connection poprawione.

--- src/main/java/pl/put/poznan/networkanalyzer/model/Connection.java
+++ src/main/java/pl/put/poznan/networkanalyzer/model/Connection.java
@@ -3,17 +3,13 @@ package pl.put.poznan.networkanalyzer.model;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import NodeType.java
 
 
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Node {
-	int id;
-	string name;
-	NodeType type;
-	ArrayList outgoing = new ArrayList();
-	ArrayList incoming = new ArrayList();	
+public class Connection {
+	int from;
+	int to;
+	int value;
 }
-
--- src/main/java/pl/put/poznan/networkanalyzer/model/NodeType.java
+++ src/main/java/pl/put/poznan/networkanalyzer/model/NodeType.java
@@ -3,17 +3,13 @@ package pl.put.poznan.networkanalyzer.model;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import NodeType.java
 
 
 @Getter
 @Setter
 @EqualsAndHashCode
-public class Node {
-	int id;
-	string name;
-	NodeType type;
-	ArrayList outgoing = new ArrayList();
-	ArrayList incoming = new ArrayList();	
+public enum NodeType {
+	entry, 
+	exit, 
+	regular;
 }
-
