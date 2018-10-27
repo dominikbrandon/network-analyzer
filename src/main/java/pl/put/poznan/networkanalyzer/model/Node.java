914abe5e48915cf996bcfe85237454a6580f072c
Pawel Myszkowski <pawel.myszkowsky@gmail.com>
2018-10-27 20:26:20 +0200

    Stworzenie klas Node i Connection

--- src/main/java/pl/put/poznan/networkanalyzer/model/Node.java
+++ src/main/java/pl/put/poznan/networkanalyzer/model/Node.java
@@ -4,8 +4,26 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+public enum typ {
+	entry, 
+	exit, 
+	regular;
+}
+
+
 @Getter
 @Setter
 @EqualsAndHashCode
 public class Node {
+	int id;
+	string name;
+	typ type;
+	ArrayList outgoing = new ArrayList();
+	ArrayList incoming = new ArrayList();	
+	
+}
+public class Connection {
+	int from;
+	int to;
+	int value;
 }
