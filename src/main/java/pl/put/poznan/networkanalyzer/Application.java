9ecb23e536a4c47e93af9743189a206588adfff7
Dominik Grzelak <grzelak.dominik@gmail.com>
2018-11-01 11:04:22 +0100

    Add Swagger

--- src/main/java/pl/put/poznan/networkanalyzer/Application.java
+++ src/main/java/pl/put/poznan/networkanalyzer/Application.java
@@ -3,13 +3,29 @@ package pl.put.poznan.networkanalyzer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @Slf4j
 public class Application {
 
     public static void main(String[] args) {
         log.debug("Application starting...");
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
