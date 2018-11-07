95077f893b9d0436a420de27a70bf05f70c978c6
Dominik Grzelak <grzelak.dominik@gmail.com>
2018-11-07 20:26:39 +0100

    Refactor compute() and getCheapestOutgoing()

--- src/main/java/pl/put/poznan/networkanalyzer/algorithms/GreedyAlgorithm.java
+++ src/main/java/pl/put/poznan/networkanalyzer/algorithms/GreedyAlgorithm.java
@@ -20,86 +20,77 @@ public class GreedyAlgorithm {
         this.nodeService = nodeService;
     }
 
-    public Node getEntry(){
+    public AlgorithmResult compute() {
+        Node currentNode = getEntry();
+        Node exit = getExit();
+        AlgorithmResult result = new AlgorithmResult();
+        result.nodes.add(currentNode);
+
+        Long currentId = currentNode.getId();
+        Long exitId = exit.getId();
+        while (!exitId.equals(currentId)) {
+            Connection nextConnection = getCheapestOutgoing(currentNode);
+            currentNode = nextConnection.getId().getTo();
+            currentId = currentNode.getId();
+
+            result.totalValue += nextConnection.getValue();
+            result.nodes.add(currentNode);
+        }
+        return result;
+    }
+
+    private Connection getCheapestOutgoing(Node examined) {
+        List<Connection> successors = examined.getOutgoing();
+        Connection cheapest = successors.get(0);
+        for (Connection successor : successors) {
+            if (successor.getValue() < cheapest.getValue()) {
+                cheapest = successor;
+            }
+        }
+        return cheapest;
+    }
+
+    private Node getEntry() {
         List<Node> listOfNodes = nodeService.getAll();
         Node entry = new Node();
         entry.setType(NodeType.REGULAR);
 
-        for(int i=0 ; i< listOfNodes.size() ; i++) {
+        for (int i = 0; i < listOfNodes.size(); i++) {
 
             if (listOfNodes.get(i).getType() == NodeType.ENTRY) {
-                if(entry.getType() == NodeType.ENTRY){
+                if (entry.getType() == NodeType.ENTRY) {
                     throw new RuntimeException("There can't be more than 1 Node with ENTRY type");
                 }
                 entry = listOfNodes.get(i);
             }
         }
 
-        if (entry.getType() == NodeType.REGULAR){
+        if (entry.getType() == NodeType.REGULAR) {
             throw new RuntimeException("There must be 1 Node with ENTRY type");
-        }else{
+        } else {
             return entry;
         }
     }
 
-    public Node getExit(){
+    private Node getExit() {
         List<Node> listOfNodes = nodeService.getAll();
         Node exit = new Node();
         exit.setType(NodeType.REGULAR);
 
-        for(int i=0 ; i< listOfNodes.size() ; i++) {
+        for (int i = 0; i < listOfNodes.size(); i++) {
 
             if (listOfNodes.get(i).getType() == NodeType.EXIT) {
-                if(exit.getType() == NodeType.EXIT){
+                if (exit.getType() == NodeType.EXIT) {
                     throw new RuntimeException("There can't be more than 1 Node with EXIT type");
                 }
                 exit = listOfNodes.get(i);
             }
         }
 
-        if (exit.getType() == NodeType.REGULAR){
+        if (exit.getType() == NodeType.REGULAR) {
             throw new RuntimeException("There must be 1 Node with EXIT type");
-        }else{
+        } else {
             return exit;
         }
-    }
-
-
-    public Connection getCheapestOutgoing(Node examined){
-
-        List<Connection> successors = examined.getOutgoing();       //make it royal
-        Connection cheapest = successors.get(0);                    //initiate it with first connection
-        Connection comparing;
-
-        for(int i = 0; i < successors.size(); i++){
-            comparing = successors.get(i);
-            if(cheapest.getValue() >= comparing.getValue()){
-                cheapest = comparing;
-            }
-        }
-
-        return cheapest;
-    }
-
-
-    public AlgorithmResult compute() {
-
-        Node first = getEntry();
-        Node last = getExit();
-        AlgorithmResult actualResult = new AlgorithmResult();           //greedyAlgorithm result will be stored here
-                actualResult.nodes.add(first);          //initiate it with first element (entry)
-                actualResult.totalValue = 0;
-        Connection nextConnection ;                                          //variable which will be holding next Node on the path
-
-        while((actualResult.nodes.getLast()).getId() != last.getId()){       //while we haven't reached the exit Node get cheapest next Node
-            nextConnection = getCheapestOutgoing(actualResult.nodes.getLast());
-            actualResult.totalValue += nextConnection.getValue();
-            actualResult.nodes.add(nextConnection.getId().getTo());
-        }
-
-        //ToDo
-        // - create class AlgorithmResult in another file
-
-        return actualResult;
     }
 }
